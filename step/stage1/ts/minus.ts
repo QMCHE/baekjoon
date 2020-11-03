124928e9a33879249cc798a23f087efb14e9d6af
QMCHE <39144276+QMCHE@users.noreply.github.com>
2020-11-03 09:40:57 +0900

    [EDIT] 1001 A-B
    Number: 1001
    Name: A-B
    Langauge: Typescript
    
    Declare parameter type

--- step/stage1/ts/minus.ts
+++ step/stage1/ts/minus.ts
@@ -1,6 +1,6 @@
 const stdin = process.openStdin()
 
-stdin.addListener("data", d => {
+stdin.addListener("data", (d: Buffer) => {
     const [a, b] = d.toString().split(" ").map(Number)
     console.log(a-b)
 })
